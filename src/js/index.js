b1d671c2f19c3513cb0f25f1ac99be3c37b25490
Your Name <you@example.com>
2019-03-08 17:57:35 +0100

    Added pagination to results

--- src/js/index.js
+++ src/js/index.js
@@ -32,7 +32,17 @@ const controlSearch = async () => {
     searchView.renderResults(state.search.result);
   }
 };
+
 elements.searchForm.addEventListener("submit", e => {
   e.preventDefault();
   controlSearch();
+});
+
+elements.searchResPages.addEventListener("click", e => {
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
 });
